47d7cdf9d7d264289eb3c8a7e9b6d2b9186394e8
72 <72sevenzy2@gmail.com>
2026-04-19 18:59:28 +0500

    refactor: remove redundant error checks (in the if statement where headers are set)

--- main.go
+++ main.go
@@ -53,15 +53,24 @@ func main() {
 	if len(headers) > 0 {
 		for _, h := range headers { // headers returns a string array
 			parts := strings.SplitN(h, ":", 2) // splitting each header by key:value pairs
-			if len(parts) == 2 {
-				req.Header.Add(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])) // trim the input if any redundant " " (spaces) are included when user sets headers
-			}
-
-			// error handling if give invalid header inputs
-			if len(parts) != 2 || len(parts) > 2 {
+			if len(parts) != 2 {
 				log.Fatalf("invalid input type %s", h)
 				return
 			}
+			req.Header.Add(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])) // trimSpace removes any un-needed spaces in the input if present.
+
+
+			// optimised code above ^.
+
+			// if len(parts) == 2 {
+			// 	req.Header.Add(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])) // trim the input if any redundant " " (spaces) are included when user sets headers
+			// }
+
+			// // error handling if give invalid header inputs
+			// if len(parts) != 2 || len(parts) > 2 {
+			// 	log.Fatalf("invalid input type %s", h)
+			// 	return
+			// }
 		}
 	}
 
