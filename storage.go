94c7bfa9de3d53e960bf5c1f26d8bd349e3c7cec
72 <72sevenzy2@gmail.com>
2026-05-13 16:57:21 +0500

    feat: add map to hold user input data + add utility GET/SET methods to set data.

--- storage.go
+++ storage.go
@@ -1,7 +1,36 @@
+// to hold user inputs during session mode - storing variables in maps to then be retrieved later using dynamic lookups.
+
 package main
 
 type key interface {
-	string | int
+	string | int // only supports these 2 types for now
 }
 
-type Data_storage[k key] map[k]string // using generics to support only types string and int as key name.
+type data[k key] struct {
+	data_storage map[k]string // using generics to support only types string and int as key name.4
+}
+
+// new db
+func NewStore[k key]() *data[k] {
+	return &data[k]{
+		make(map[k]string),
+	}
+}
+
+// utility get/set functions for data map:
+
+// for strings
+func (d *data[k]) Get(keyname k) (string, bool) {
+	val, ok := d.data_storage[keyname]
+	return val, ok
+}
+
+func (d *data[k]) Set(keyname k, value string) bool {
+	// validate if value exists first
+	if value != "" {
+		d.data_storage[keyname] = value
+		return true
+	} else {
+		return false // and then handle error from the module calling this method.
+	}
+}
